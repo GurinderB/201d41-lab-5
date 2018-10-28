18261474d9eea26bf526d9ef4558fa6c8afc06d5
Gurinder Batth <gurinder@gmx.com>
2018-10-28 12:50:35 -0700

    Question 4 2nd commit

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  return [sum((sum(a,b)[0]), c)[0], multiply((multiply(a,b)[0]), c)[0], a + ' and ' + b + ' and ' + c + ' sum to ' + sum((sum(a,b)[0]), c)[0] + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply((multiply(a,b)[0]), c)[0] + '.'];
+  return [sum((sum(a, b)[0]), c)[0], multiply((multiply(a, b)[0]), c)[0], a + ' and ' + b + ' and ' + c + ' sum to ' + sum((sum(a, b)[0]), c)[0] + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply((multiply(a, b)[0]), c)[0] + '.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -70,7 +70,21 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var val = 0;
+  if (sumArr instanceof Array) {
+    for (var i = 0; i < sumArr.length; i++) {
+      val = sum(val, sumArr[i])[0];
+      // console.log(sumArr.length);
+      // console.log(val);
+      //console.log(sum());
+    }
+  }
+  //console.log(val);
+  //console.log(sum());
+  //return (val);
+  console.log(sumArr);
+  console.log(testArray);
+  return [testArray];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -91,7 +105,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var val = 1;
+  if (multArr instanceof Array) {
+    for (var i = 0; i < multArr.length; i++) {
+      val = multiply(val, multArr[i])[0];
+      return [val];
+    }
+  }
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
